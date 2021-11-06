6db0bdb904e3150395b340345e846068da297183
Volodymyr Varkhol <proteriusgrid@gmail.com>
2021-11-06 18:09:37 +0200

    Git 1.1 was added

--- src/test_1.java
+++ src/test_1.java
@@ -1,5 +1,7 @@
 public class test_1 {
     public static void main( String[] args ) {
+
         System.out.println("Hello, World!!! Git");
+        System.out.println("Hello, World and Git");
     }
 }
